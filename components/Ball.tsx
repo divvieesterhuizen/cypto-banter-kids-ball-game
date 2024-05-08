d42ce05ce6f9d0ac9bfc0466db49371dd189ceb4
Divvie Esterhuizen <divvieesterhuizen@Divvies-MacBook-Pro.local>
2024-05-08 13:51:16 +0200

    setup ball on screen, setup moveing of ball with MIN and MAX as screen start and end positions

--- components/Ball.tsx
+++ components/Ball.tsx
@@ -16,75 +16,95 @@ import { Gyroscope } from "expo-sensors";
 
 import { BallProps } from "../types";
 
-const Ball: FC<BallProps> = ({  }) => {
+const Ball: FC<BallProps> = ({}) => {
   const screenHeight = Dimensions.get("window").height;
   const screenWidth = Dimensions.get("window").width;
 
-  const [isBallSizeSmall, setIsBallSizeSmall] = useState<Boolean>(false);
+  const [isBallSizeSmall, setIsBallSizeSmall] = useState<boolean>(false);
   const ballSize = isBallSizeSmall ? screenWidth * 0.1 : screenWidth * 0.3;
 
-  const positionX = useSharedValue(0);
-  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(screenWidth / 2 - ballSize / 2);
+  const positionY = useSharedValue(screenHeight / 2 - ballSize / 2);
 
-   const handleToggleBallSize = () => {
-     setIsBallSizeSmall(!isBallSizeSmall);
-   };
+  const handleToggleBallSize = () => {
+    setIsBallSizeSmall(!isBallSizeSmall);
+  };
 
-   const animatedStyles = useAnimatedStyle(() => ({
-     width: ballSize,
-     height: ballSize,
-     borderRadius: ballSize / 2,
-     backgroundColor: "red",
-     position: "absolute",
-     top: positionY.value,
-     left: positionX.value,
-   }));
+  const animatedStyles = useAnimatedStyle(() => ({
+    width: ballSize,
+    height: ballSize,
+    borderRadius: ballSize / 2,
+    backgroundColor: "#FF0000",
+    position: "absolute",
+    top: positionY.value,
+    left: positionX.value,
+  }));
 
+  const moveBall = (event: { x: number; y: number; z: number }) => {
+    positionX.value = Math.min(
+      Math.max(positionX.value + event.y, 0),
+      screenWidth - ballSize
+    );
+    positionY.value = Math.min(
+      Math.max(positionY.value + event.x, 0),
+      screenHeight - ballSize
+    );
+  };
 
+  const moveButtonToCenterScreen = () => {
+    positionX.value = screenWidth / 2 - ballSize / 2;
+    positionY.value = screenHeight / 2 - ballSize / 2;
+  };
 
-   return (
-     <>
-       <Animated.View style={[styles.box, animatedStyles]} />
-       <View style={styles.container}>
-         <Button onPress={handleToggleBallSize} title="Toggle Ball Size" />
-       </View>
-     </>
-   );
+  useEffect(() => {
+    Gyroscope.setUpdateInterval(16);
+    const subscription = Gyroscope.addListener((data) => {
+      moveBall(data);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  return (
+    <>
+      <Animated.View style={[animatedStyles]}></Animated.View>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={[styles.button]}
+          onPress={handleToggleBallSize}
+        >
+          <Text style={styles.buttonText}>Toggle Ball Size</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: "#016AAA" }]}
+          onPress={moveButtonToCenterScreen}
+        >
+          <Text style={styles.buttonText}>Center Ball On Screen</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 20,
   },
-  box: {
-    height: 120,
-    width: 120,
-    backgroundColor: "#b58df1",
-    borderRadius: 20,
-    marginVertical: 50,
+  button: {
+    padding: 10,
+    backgroundColor: "#015FFF",
+    borderRadius: 5,
+    marginBottom: 10,
   },
-  // text: {
-  //   textAlign: "center",
-  // },
-  // buttonContainer: {
-  //   flexDirection: "row",
-  //   alignItems: "stretch",
-  //   marginTop: 15,
-  // },
-  // button: {
-  //   flex: 1,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   backgroundColor: "#eee",
-  //   padding: 10,
-  // },
-  // middleButton: {
-  //   borderLeftWidth: 1,
-  //   borderRightWidth: 1,
-  //   borderColor: "#ccc",
-  // },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
 });
 
 export default Ball;
